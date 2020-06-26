0ed80d3e1b414cfde0d5fdfb40e0877f227ea248
Xinyang Li <soybeanyoung@gmail.com>
2020-06-26 01:39:24 -0400

    split from polariton project

---
+++ REAME.md
@@ -0,0 +1,4 @@
+# Factors to convert atomic units
+
+My own simple package to storage some conversion factors that are useful to
+molecular dynamics simulations
---
+++ src/Constants.jl
@@ -0,0 +1,12 @@
+module Constants
+
+export au2wn, au2ev, au2kelvin, au2kcal, amu2au, au2fs
+
+au2wn = 219474.63068
+au2ev = 27.2113961
+au2kelvin = 3.15775e5
+au2kcal = 627.509
+amu2au = 1836.0
+au2fs = 2.41888e-2
+
+end # module
