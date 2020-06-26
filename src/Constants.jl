902a069f58add5680b348e7f8364c8a165f0d154
Xinyang Li <soybeanyoung@gmail.com>
2020-06-26 01:52:35 -0400

    all variables should be set to const

--- src/Constants.jl
+++ src/Constants.jl
@@ -2,11 +2,11 @@ module Constants
 
 export au2wn, au2ev, au2kelvin, au2kcal, amu2au, au2fs
 
-au2wn = 219474.63068
-au2ev = 27.2113961
-au2kelvin = 3.15775e5
-au2kcal = 627.509
-amu2au = 1836.0
-au2fs = 2.41888e-2
+const au2wn = 219474.63068
+const au2ev = 27.2113961
+const au2kelvin = 3.15775e5
+const au2kcal = 627.509
+const amu2au = 1836.0
+const au2fs = 2.41888e-2
 
 end # module
